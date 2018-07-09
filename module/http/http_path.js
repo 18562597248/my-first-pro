526595c12947e58ba606d15690175844189d63c7
changjp <changjp@huadi.info>
2018-07-09 10:18:14 +0800

    change directory

--- module/http/http_path.js
+++ module/http/http_path.js
@@ -7,8 +7,8 @@ var workDir = path.resolve('/');        // 'e:\'
 
 
 // 组合完整的文件路径:当前目录+...+targetFile:
-var filePath = path.join(workDir, 'NodeJsWorkspace', 'git','personal-UI','file','hard.txt');
-// 'e:\NodeJsWorkspace\git\personal-UI\file\hard.txt'
+var filePath = path.join(workDir, 'NodeJsWorkspace', 'git','my-first-pro','file','hard.txt');
+// 'e:\NodeJsWorkspace\git\my-first-pro\file\hard.txt'
 
 console.log('filePath : '+filePath);
 
